c6906b696f471a11b637515e1339c1982ced86a3
Przemek Lewandowski <przemolew@interia.pl>
2021-03-03 21:51:32 +0100

    Added test cases to GenderServiceTest

--- src/test/java/com/myapp/detector/service/GenderServiceTest.java
+++ src/test/java/com/myapp/detector/service/GenderServiceTest.java
@@ -1,5 +1,6 @@
 package com.myapp.detector.service;
 
+import com.myapp.detector.domain.Result;
 import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -11,9 +12,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -21,6 +19,12 @@ import static org.mockito.Mockito.when;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith(MockitoExtension.class)
 class GenderServiceTest implements WithAssertions {
+
+    private static final String MALE_NAME = "Andrzej";
+    private static final String FEMALE_NAME = "Anita";
+    private static final String INCONCLUSIVE_NAME = "Damian";
+    private static final String NOT_EXISTING_NAME = "Noname";
+    private static final String NAME_NOT_FOUND_MESSAGE = "There is no name in both files.";
 
     @MockBean
     private NameCounter nameCounter;
@@ -33,17 +37,50 @@ class GenderServiceTest implements WithAssertions {
     }
 
     @Test
-    void shouldDetectMaleGender() throws IOException, URISyntaxException {
-        // given
-        String name = "Andrzej";
-
+    void shouldDetectMaleGender() {
         // when
         when(nameCounter.countFemaleNames(any())).thenReturn(0L);
         when(nameCounter.countMaleNames(any())).thenReturn(1L);
-        ResponseEntity<String> responseEntity = genderService.detectGenderByFirstToken(name);
+        ResponseEntity<String> responseEntity = genderService.detectGenderByFirstToken(MALE_NAME);
 
         // then
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isEqualTo("MALE");
+        assertThat(responseEntity.getBody()).isEqualTo(Result.MALE.name());
+    }
+
+    @Test
+    void shouldDetectFemaleGender() {
+        // when
+        when(nameCounter.countFemaleNames(any())).thenReturn(1L);
+        when(nameCounter.countMaleNames(any())).thenReturn(0L);
+        ResponseEntity<String> responseEntity = genderService.detectGenderByFirstToken(FEMALE_NAME);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isEqualTo(Result.FEMALE.name());
+    }
+
+    @Test
+    void shouldDetectInconclusiveGender() {
+        // when
+        when(nameCounter.countFemaleNames(any())).thenReturn(1L);
+        when(nameCounter.countMaleNames(any())).thenReturn(1L);
+        ResponseEntity<String> responseEntity = genderService.detectGenderByFirstToken(INCONCLUSIVE_NAME);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isEqualTo(Result.INCONCLUSIVE.name());
+    }
+
+    @Test
+    void shouldReturnResponseEntityWithNotFoundHttpStatus() {
+        // when
+        when(nameCounter.countFemaleNames(any())).thenReturn(0L);
+        when(nameCounter.countMaleNames(any())).thenReturn(0L);
+        ResponseEntity<String> responseEntity = genderService.detectGenderByFirstToken(NOT_EXISTING_NAME);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(responseEntity.getBody()).isEqualTo(NAME_NOT_FOUND_MESSAGE);
     }
 }
